53783846bf0a308a166ea3f56fc5dd61c87a2f4c
Zhaobowen <1877296512@qq.com>
2025-03-26 23:02:26 +0800

    精简，省略无用的库

--- UNet/train.py
+++ UNet/train.py
@@ -1,14 +1,9 @@
-import argparse
-import logging
 import os
 import random
-import sys
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.transforms as transforms
-import torchvision.transforms.functional as TF
-from pathlib import Path
 from torch import optim
 from torch.utils.data import DataLoader, random_split
 
